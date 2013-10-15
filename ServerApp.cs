1fdb7d576f25dda1dcb46eeaf7575e4ee5434a30
jefferson <jeff.tokurah@gmail.com>
2013-10-15 01:09:37 -0500

    More implementation of the server app

--- ServerApp.cs
+++ ServerApp.cs
@@ -53,16 +53,24 @@ namespace PaymentServer
             JsonObjectCollection transactions;
 
             //Define outgoing JSON message structures 
+            JsonStringValue Accept_Encoding = new JsonStringValue("Accept-Encoding", "gzip,deflate,sdch");
+            JsonStringValue Cookie = new JsonStringValue("Cookie", "_gauges_unique_month=1; _gauges_unique_year=1; _gauges_unique=1");
+            JsonStringValue Accept_Language = new JsonStringValue("Accept-Language", "en-CA,en-GB,en-US;q=0.8,en;q=0.6");
+            JsonStringValue Accept = new JsonStringValue("Accept", "application/json, text/json");
+            JsonStringValue Host = new JsonStringValue("Host", "paymentserver.dynu.com");
+            JsonStringValue Referer = new JsonStringValue("Referer", "https://paymentserver.dynu.com");
+            JsonStringValue Connection = new JsonStringValue("Connection", "close");
+            JsonStringValue User_Agent = new JsonStringValue("User-Agent", "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.69 Safari/537.36");
             headers = new JsonObjectCollection();
             headers.Name = "headers";
-            headers.Add(new JsonStringValue("Accept-Encoding", "gzip,deflate,sdch"));
-            headers.Add(new JsonStringValue("Cookie", "_gauges_unique_month=1; _gauges_unique_year=1; _gauges_unique=1"));
-            headers.Add(new JsonStringValue("Accept-Language", "en-CA,en-GB,en-US;q=0.8,en;q=0.6"));
-            headers.Add(new JsonStringValue("Accept", "application/json, text/json"));
-            headers.Add(new JsonStringValue("Host", "paymentserver.dynu.com"));
-            headers.Add(new JsonStringValue("Referer", "https://paymentserver.dynu.com"));
-            headers.Add(new JsonStringValue("Connection", "close"));
-            headers.Add(new JsonStringValue("User-Agent", "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.69 Safari/537.36"));
+            headers.Add(Accept_Encoding);
+            headers.Add(Cookie);
+            headers.Add(Accept_Language);
+            headers.Add(Accept);
+            headers.Add(Host);
+            headers.Add(Referer);
+            headers.Add(Connection);
+            headers.Add(User_Agent);
 
             JsonNumericValue code = new JsonNumericValue("code", -1);
             JsonBooleanValue request = new JsonBooleanValue("request", false);
@@ -75,73 +83,100 @@ namespace PaymentServer
             messageType.Add(response);
             messageType.Add(details);
 
+            JsonStringValue bankCode = new JsonStringValue("bankCode", "");
+            JsonStringValue accountNum = new JsonStringValue("accountNum", "");
+            JsonStringValue accountPWD = new JsonStringValue("accountPWD", "");
+            JsonNumericValue acctBalance = new JsonNumericValue("acctBalance", -1);
             JsonObjectCollection account = new JsonObjectCollection();
             account.Name = "account";
-            account.Add(new JsonStringValue("bankCode", ""));
-            account.Add(new JsonStringValue("accountNum", ""));
-            account.Add(new JsonStringValue("accountPWD", ""));
-            account.Add(new JsonNumericValue("acctBalance", -1));
+            account.Add(bankCode);
+            account.Add(accountNum);
+            account.Add(accountPWD);
+            account.Add(acctBalance);
 
+            JsonNumericValue POSHWID = new JsonNumericValue("POSHWID", -1);
+            JsonStringValue currentDK = new JsonStringValue("currentDK", "");
+            JsonStringValue nextDK = new JsonStringValue("nextDK", "");
             JsonObjectCollection hardwareInfo = new JsonObjectCollection();
             hardwareInfo.Name = "hardwareInfo";
-            hardwareInfo.Add(new JsonNumericValue("POSHWID", -1));
-            hardwareInfo.Add(new JsonStringValue("currentDK", ""));
-            hardwareInfo.Add(new JsonStringValue("nextDK", ""));
+            hardwareInfo.Add(POSHWID);
+            hardwareInfo.Add(currentDK);
+            hardwareInfo.Add(nextDK);
 
+            JsonStringValue username = new JsonStringValue("username", "");
+            JsonStringValue password = new JsonStringValue("password", "");
             JsonObjectCollection userID = new JsonObjectCollection();
             userID.Name = "userID";
-            userID.Add(new JsonStringValue("username", ""));
-            userID.Add(new JsonStringValue("password", ""));
+            userID.Add(username);
+            userID.Add(password);
 
+            JsonStringValue userType = new JsonStringValue("userType", "");
+            JsonStringValue transactionHistory = new JsonStringValue("transactionHistory", "");
             user = new JsonObjectCollection();
             user.Name = "user";
-            user.Add(new JsonStringValue("userType", ""));
-            user.Add(new JsonStringValue("transactionHistory", ""));
+            user.Add(userType);
+            user.Add(transactionHistory);
             user.Add(account);
             user.Add(hardwareInfo);
             user.Add(userID);
 
+            JsonNumericValue merchantID = new JsonNumericValue("merchantID", -1);
+            JsonStringValue merchantName = new JsonStringValue("merchantName", "");
             merchant = new JsonObjectCollection();
             merchant.Name = "merchant";
-            merchant.Add(new JsonNumericValue("merchantID", -1));
-            merchant.Add(new JsonStringValue("merchantName", ""));
+            merchant.Add(merchantID);
+            merchant.Add(merchantName);
 
+            JsonStringValue custUsername = new JsonStringValue("custUsername", "");
+            JsonStringValue custPWD = new JsonStringValue("custPWD", "");
             customer = new JsonObjectCollection();
             customer.Name = "customer";
-            customer.Add(new JsonStringValue("custUsername", ""));
-            customer.Add(new JsonStringValue("custPWD", ""));
+            customer.Add(custUsername);
+            customer.Add(custPWD);
 
-
+            JsonNumericValue year = new JsonNumericValue("year", -1);
+            JsonNumericValue month = new JsonNumericValue("month", -1);
+            JsonNumericValue day = new JsonNumericValue("day", -1);
             JsonObjectCollection transactionDate = new JsonObjectCollection();
             transactionDate.Name = "transactionDate";
-            transactionDate.Add(new JsonNumericValue("year", -1));
-            transactionDate.Add(new JsonNumericValue("month", -1));
-            transactionDate.Add(new JsonNumericValue("day", -1));
+            transactionDate.Add(year);
+            transactionDate.Add(month);
+            transactionDate.Add(day);
 
+            JsonNumericValue hour = new JsonNumericValue("hour", -1);
+            JsonNumericValue minute = new JsonNumericValue("minute", -1);
+            JsonNumericValue second = new JsonNumericValue("second", -1);
             JsonObjectCollection transactionTime = new JsonObjectCollection();
             transactionTime.Name = "transactionTime";
-            transactionTime.Add(new JsonNumericValue("hour", -1));
-            transactionTime.Add(new JsonNumericValue("minute", -1));
-            transactionTime.Add(new JsonNumericValue("second", -1));
+            transactionTime.Add(hour);
+            transactionTime.Add(minute);
+            transactionTime.Add(second);
 
-            JsonObjectCollection merchantID = new JsonObjectCollection();
-            merchantID.Name = "merchantID";
-            merchantID.Add(new JsonStringValue("username", ""));
+            JsonStringValue merchantUsername = new JsonStringValue("merchantUsername", "");
+            JsonStringValue merchantPWD = new JsonStringValue("merchantPWD", "");
+            JsonObjectCollection merchantIdent = new JsonObjectCollection();
+            merchantIdent.Name = "merchantIdent";
+            merchantIdent.Add(merchantUsername);
+            merchantIdent.Add(merchantPWD);
 
+            JsonNumericValue transactionID = new JsonNumericValue("transactionID", -1);
+            JsonNumericValue debitAmount = new JsonNumericValue("debitAmount", -1);
+            JsonNumericValue creditAmount = new JsonNumericValue("creditAmount", -1);
+            JsonNumericValue balance = new JsonNumericValue("balance", -1);
+            JsonNumericValue receiptNo = new JsonNumericValue("receiptNo", -1);
             transactions = new JsonObjectCollection();
             transactions.Name = "transactions";
-            transactions.Add(new JsonNumericValue("transactionID", -1));
-            transactions.Add(new JsonNumericValue("debitAmount", -1));
-            transactions.Add(new JsonNumericValue("creditAmount", -1));
-            transactions.Add(new JsonNumericValue("balance", -1));
-            transactions.Add(new JsonNumericValue("receiptNo", -1));
+            transactions.Add(transactionID);
+            transactions.Add(debitAmount);
+            transactions.Add(creditAmount);
+            transactions.Add(balance);
+            transactions.Add(receiptNo);
             transactions.Add(transactionDate);
             transactions.Add(transactionTime);
             transactions.Add(merchantID);
 
             //create JSON object
-            JsonObjectCollection defineResponse = new JsonObjectCollection();
-            
+            JsonObjectCollection defineResponse = new JsonObjectCollection();     
 
             if (method == "GET")
             {
@@ -151,10 +186,9 @@ namespace PaymentServer
                 defineResponse.Insert(0, headers);
                 defineResponse.Add(messageType);
                 defineResponse.Add(user);
-                defineResponse.Add(merchant);
+                defineResponse.Add(merchantIdent);
                 defineResponse.Add(customer);
                 defineResponse.Add(transactions);
-            
             }
 
             /* 
@@ -174,28 +208,29 @@ namespace PaymentServer
                 switch (transactionCode)
                 {
                     case ((int)clientIncomingCodeEnum.IN_CODE_LOGIN_REQ):
-                        string username = (string)received.SelectToken("custUsername");
-                        string password = (string)received.SelectToken("custPWD");
-                        Console.WriteLine("custUsename: {0}", username);
-                        Console.WriteLine("custPWD: {0}", password);
+                        string uName = (string)received.SelectToken("custUsername");
+                        string PWD = (string)received.SelectToken("custPWD");
+                        Console.WriteLine("custUsename: {0}", uName);
+                        Console.WriteLine("custPWD: {0}", PWD);
 
                         //JT: The authentication logic shoud come here. For now we will authenticate all requests
                         if (true)  //JT:HACK - This should evaluate logic that checks if the user is authentiicated or not
                         {
                             messageType = insert(messageType, code, new JsonNumericValue("code", (int)clientOutgoingCodeEnum.OUT_CODE_LOGIN_SUCCESS));
-                            messageType = insert(messageType, response,  new JsonBooleanValue("response", true));
-                            messageType = insert(messageType, details,  new JsonStringValue("details", "Success"));
+                            messageType = insert(messageType, response, new JsonBooleanValue("response", true));
+                            messageType = insert(messageType, request, new JsonBooleanValue("request", false));
+                            messageType = insert(messageType, details, new JsonStringValue("details", "Success"));
                         }
                         else{
                             messageType = insert(messageType, code, new JsonNumericValue("code", (int)clientOutgoingCodeEnum.OUT_CODE_LOGIN_FAILURE));
                             messageType = insert(messageType, response, new JsonBooleanValue("response", true));
+                            messageType = insert(messageType, request, new JsonBooleanValue("request", false));
                             messageType = insert(messageType, details, new JsonStringValue("details", "Invalid username and passowrd combination"));
                         }
                         //build response content from already defined JSON Objects               
                         defineResponse.Insert(0, headers);
                         defineResponse.Add(messageType);
 
-                        Console.WriteLine("Line 3");
                         JObject resp = JObject.Parse(defineResponse.ToString());
                         Console.WriteLine("resp: \n{0}", defineResponse.ToString());
                         //
@@ -218,16 +253,6 @@ namespace PaymentServer
             byte[] message = JsonStringToByteArray(completeResponse.ToString());
             p.sslStream.Write(message);
         }
-
-        public static void handleMobileRequest(HttpProcessor p, StreamReader inputData)
-        {
-
-        }
-        public static void handleWebRequest(HttpProcessor p, StreamReader inputData)
-        {
-
-        }
-
 
         public static byte[] JsonStringToByteArray(string jsonString)
         {
